fe55c3d0afb681c2bf2f3be2aa5bfaa9c4273fd5
Lauri Saarni <lauri.saarni@gmail.com>
2021-02-11 11:11:54 +0700

    remove player.module.css as it is not used

--- src/components/icon-with-text.js
+++ src/components/icon-with-text.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-const IconWithText = ({children, text }) => (
-    <div className="flex items-center mr-4">
-        {children}
-        <p className="ml-2 text-sm">{text}</p>
-    </div>
-)
+const IconWithText = ({ children, text }) => (
+  <div className="flex items-center mr-4">
+    {children}
+    <p className="ml-2 text-sm">{text}</p>
+  </div>
+);
 
 export default IconWithText;
